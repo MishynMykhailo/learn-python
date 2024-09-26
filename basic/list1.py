b23df70078ea9cbfbe2e36d0a1335f180325a74d
MishynMykhailo <91686513+MishynMykhailo@users.noreply.github.com>
2024-09-27 00:04:01 +0300

    i don't know what is it

--- basic/list1.py
+++ basic/list1.py
@@ -21,8 +21,12 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
+  count = 0
+  for word in words:
+    if len(word) >=2:
+      if word[0] == word[(len(word)-1)]:
+        count += 1
+  return count
 
 
 # B. front_x
@@ -33,10 +37,9 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-  # +++your code here+++
+  sorted_words = sorted(words,key=personal_x_sort)
+  print(sorted_words)
   return
-
-
 
 # C. sort_last
 # Given a list of non-empty tuples, return a list sorted in increasing
